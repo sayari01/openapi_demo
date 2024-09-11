f901194337a494691ef65424f35b147428f931ad
sayari01 <100856549+sayari01@users.noreply.github.com>
2024-09-11 17:22:36 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplications.class, args);
 	}
 
 	@RequestMapping("/")
